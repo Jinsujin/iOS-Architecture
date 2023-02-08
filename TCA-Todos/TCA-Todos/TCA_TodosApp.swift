031f0ef235876362e5e2c69988a505269315b5d5
Jinsujin <sylpid003@gmail.com>
2023-02-08 23:05:21 +0900

    Init: 프로젝트 생성

---
+++ TCA-Todos/TCA-Todos/TCA_TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_TodosApp.swift
+//  TCA-Todos
+//
+//  Created by Sujin Jin on 2023/02/08.
+//
+
+import SwiftUI
+
+@main
+struct TCA_TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
